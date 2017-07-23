78b93c2dc48821a0c2efc06a801fcfffec5869f3
Akivamu <akivamu@gmail.com>
2017-07-23 16:17:24 +0700

    command line

--- src/main/java/com/akivamu/gdx/Main.java
+++ src/main/java/com/akivamu/gdx/Main.java
@@ -1,7 +1,70 @@
 package com.akivamu.gdx;
 
-public class Main {
-    public static void main(String[] args) {
+import com.akivamu.gdx.crypto.Crypto;
+import com.akivamu.gdx.crypto.SimpleXorCrypto;
 
+import java.io.File;
+
+public class Main {
+    private static final String OUTPUT_DIR_NAME = "encrypted";
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            printHelp();
+        } else {
+
+            // Output
+            String outputDirName = OUTPUT_DIR_NAME;
+            if (args.length == 3) {
+                outputDirName = args[2];
+            }
+
+            // Key
+            byte key = 0;
+            try {
+                key = Byte.parseByte(args[0]);
+            } catch (NumberFormatException e) {
+                shutdown("Invalid key. Must be a number from 0-255");
+            }
+
+            // Input
+            File input = new File(args[1]);
+
+            if (!input.exists()) shutdown("Input not exists");
+
+            File[] inputFiles = new File[1];
+            if (input.isDirectory()) {
+                inputFiles = input.listFiles();
+            } else {
+                inputFiles[0] = input;
+            }
+            if (inputFiles == null || inputFiles.length == 0) shutdown("Input is empty");
+
+            // Do encrypt
+            File outputDir = new File(outputDirName);
+            outputDir.mkdirs();
+            Crypto crypto = new SimpleXorCrypto(key);
+            for (File file : inputFiles) {
+                if (file.isDirectory()) continue;
+                String inputPath = file.getAbsolutePath();
+                String outputPath = outputDir.getAbsolutePath() + "/" + file.getName();
+                TextureEncryptor.encryptToFile(crypto, inputPath, outputPath);
+                System.out.println(inputPath + " -> " + outputPath);
+            }
+        }
+    }
+
+
+    private static void shutdown(String text) {
+        System.out.println(text);
+        System.exit(1);
+    }
+
+    private static void printHelp() {
+        System.out.println("Encrypt file by simple xor crypto.");
+        System.out.println("Command line arguments:");
+        System.out.println("   <key>       (required) key to encrypt (0-255)");
+        System.out.println("   <input>     (required) input file/folder");
+        System.out.println("   [output]    (optional) output folder");
     }
 }
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,48 @@
+import com.akivamu.gdx.Main;
+import org.junit.Test;
+
+public class TestMain {
+    @Test
+    public void testPrintHelp() {
+        Main.main(new String[]{
+                "-h"
+        });
+
+        Main.main(new String[]{
+                "badlogic.jpg"
+        });
+
+        Main.main(new String[]{
+                "1234",
+                "badlogic.jpg",
+        });
+    }
+
+    @Test
+    public void testSingleFile() {
+        Main.main(new String[]{
+                "123",
+                "badlogic.jpg",
+        });
+
+        Main.main(new String[]{
+                "123",
+                "badlogic.jpg",
+                "output",
+        });
+    }
+
+    @Test
+    public void testFolder() {
+        Main.main(new String[]{
+                "123",
+                ".",
+        });
+
+        Main.main(new String[]{
+                "123",
+                ".",
+                "output",
+        });
+    }
+}
